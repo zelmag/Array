2de61b2c047d5f31217df8035aa4304381e5af25
Zelma Garza <zelmagarza@utexas.edu>
2019-12-18 10:33:36 -0600

    Update twoSum.java

--- twoSum.java
+++ twoSum.java
@@ -2,17 +2,14 @@
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] ans = new int[2];
-        HashMap<Integer,Integer> seen = new HashMap<>(); //number, indices
-        for(int i = 0; i < nums.length; i++){
-            int lookingFor = target-nums[i];
-            if(seen.containsKey(lookingFor)){
-                ans[0]=seen.get(lookingFor);
-                ans[1]=i;
-            }else{
-                seen.put(nums[i],i);
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            int remTarget = target - nums[i];
+            if(map.containsKey(remTarget)){
+                return new int[]{i,map.get(remTarget)};
             }
+            map.put(nums[i],i);
         }
-        return ans;
+        return new int[2];
     }
 }
