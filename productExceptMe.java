452a3fa1355253b903e446e555c0b33af144ae57
Zelma Garza <zelmagarza@utexas.edu>
2019-12-21 00:11:34 -0600

    Update productExceptMe.java

--- productExceptMe.java
+++ productExceptMe.java
@@ -1,4 +1,4 @@
-//notice ans[i] is the product of everything before it AND everything after. 
+//notice ans[i] is the product of everything before it * everything after. 
 //O(N) time without using division
 
 class Solution {
@@ -6,11 +6,13 @@ class Solution {
         int[] ans = new int[nums.length];
         int forwardProd = 1;
         for(int i = 0; i < nums.length; i++){
+            //put in the product of everything before it 
             ans[i] = forwardProd;
             forwardProd*=nums[i]; //for the next one 
         }
         int backwardProd = 1;
         for(int i = nums.length-1; i >= 0; i--){
+            //product of everything after.
             ans[i] *= backwardProd;
             backwardProd*=nums[i]; //include it the next one 
         }
